30e249b147550c4ca964cb1f6fee494d5349aab2
Martim Lima <martim.d.lima@protonmail.com>
2022-03-21 17:37:38 +0000

    More Fixes

--- .local/bin/scripts/post-installation-script/setup.sh
+++ .local/bin/scripts/post-installation-script/setup.sh
@@ -13,31 +13,30 @@ sudo pacman -Sy
 
 # check if dialog exists
 if ! command -v dialog &> /dev/null; then
-  echo "Can't work without Dialog. Dialog must be installed to procceed."
-  printf "Would you like to install Dialog? (y/N)"
-  read -r install
-  [ "$(tr '[:upper:]' '[:lower:]' <<< "$install")" = "y" ] && sudo pacman -S dialog
- # exit 1
+  sudo pacman -S dialog
+  createdialog
+else
+  createdialog
 fi
 
-# Initializes the dialog with the specifed measurements
-cmd=(dialog --separate-output --checklist "Welcome to DotHelper. Press SPACE to toggle an option on/off." 22 76 16)
-options=(
-  1 "Update System & Install Software" off    # any option can be set to default to "on"
-  2 "Install dotfiles" off)
-  choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-  clear
+createdialog() {
+  # Initializes the dialog with the specifed measurements
+  cmd=(dialog --separate-output --checklist "Welcome to DotHelper. Press SPACE to toggle an option on/off." 22 76 16)
+  options=(
+    1 "Update System & Install Software" off    # any option can be set to default to "on"
+    2 "Install dotfiles" off)
+    choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+    clear
 
-for choice in $choices
-  do
-  case $choice in
-      1)  
-      curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh | /bin/bash
-      ;;
-    2)
-      curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh | /bin/bash
-      ;;
-    esac
-done
-
-goodbye
+  for choice in $choices
+    do
+    case $choice in
+        1)  
+        curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh | /bin/bash
+        ;;
+      2)
+        curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh | /bin/bash
+        ;;
+      esac
+  done
+}
