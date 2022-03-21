2c5b77abbf32ddf587d5c89e8df7d20b00555728
Martim Lima <martim.d.lima@protonmail.com>
2022-03-21 14:06:27 +0000

    Fix to setup.sh

--- .local/bin/scripts/post-installation-script/setup.sh
+++ .local/bin/scripts/post-installation-script/setup.sh
@@ -22,36 +22,38 @@ intro() {
 }
 
 goodbye() {
-  printf "\a\n\n%s\n" "Thanks for using dothelper. }"
+  printf "\a\n\n%s\n" "Thanks for using dothelper."
 }
 
 intro
 
-# check if dialog exists
-if ! command -v dialog; then
-  sudo pacman -Sy
+# Updating Mirros and downloading dialog
+echo "Updating Mirrors"
+sudo pacman -Sy
+
+if ! which dialog &> /dev/null; then
+  echo "Installing Dialog"
   sudo pacman -S dialog
-  echo "test"
 fi
 
 # Initializes the dialog with the specifed measurements
 cmd=(dialog --separate-output --checklist "Welcome to DotHelper. Press SPACE to toggle an option on/off." 22 76 16)
 options=(
-	1 "Update System & Install Software" off    # any option can be set to default to "on"
-	2 "Install dotfiles" off)
-	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-	clear
+  1 "Update System & Install Software" off    # any option can be set to default to "on"
+  2 "Install dotfiles" off)
+  choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+  clear
 
 for choice in $choices
-	do
-	case $choice in
-    	1)	
+  do
+  case $choice in
+      1)  
 
-			curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh | /bin/bash
-			;;
-		2)
-			curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh | /bin/bash
-			;;
+      curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh | /bin/bash
+      ;;
+    2)
+      curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh | /bin/bash
+      ;;
     esac
 done
 
