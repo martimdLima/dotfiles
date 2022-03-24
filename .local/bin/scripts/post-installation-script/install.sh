68fcb8d01afb9d893fec4ffd0f5e6b62c4fa49f4
Martim Lima <martim.d.lima@protonmail.com>
2022-03-24 13:43:35 +0000

    Test changes

--- .local/bin/scripts/post-installation-script/install.sh
+++ .local/bin/scripts/post-installation-script/install.sh
@@ -10,12 +10,12 @@ clean_up() {
   test -d "$SCRIPTS_DIR" && rm -fr "$SCRIPTS_DIR"
 }
 
-trap "clean_up $tmp_dir" EXIT
-
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/setup.sh > $SCRIPTS_DIR/setup.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh > $SCRIPTS_DIR/post_install.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh > $SCRIPTS_DIR/dot_files_config.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
 
-. $SCRIPTS_DIR/setup.sh
+#. $SCRIPTS_DIR/setup.sh
+
+trap "clean_up $tmp_dir" EXIT
 
