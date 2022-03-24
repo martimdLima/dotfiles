95ccb5ea19e5fb2e822e6b9fd898e664ec101b47
Martim Lima <martim.d.lima@protonmail.com>
2022-03-24 12:49:40 +0000

    Test changes

--- .local/bin/scripts/post-installation-script/install.sh
+++ .local/bin/scripts/post-installation-script/install.sh
@@ -8,5 +8,5 @@ curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/b
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh > $TMP_DIR/dot_files_config.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $TMP_DIR/colors.sh
 
-. setup.sh
+sudo . setup.sh
 
