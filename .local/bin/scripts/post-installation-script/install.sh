e67a4b7e9f8c08df570fd89f982d0c7756da1c59
Martim Lima <martim.d.lima@protonmail.com>
2022-03-24 13:41:20 +0000

    Test changes

--- .local/bin/scripts/post-installation-script/install.sh
+++ .local/bin/scripts/post-installation-script/install.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-SCRIPTS_PATH="$HOME/Downloads/scripts"
+SCRIPTS_DIR="$HOME/Downloads/scripts"
 
-if [! -d "$SCRIPTS_PATH" ]; then
-	mkdir $SCRIPTS_PATH
+if [! -d "$SCRIPTS_DIR" ]; then
+	mkdir $SCRIPTS_DIR
 fi
 
-TMP_DIR=$( mkdir $SCRIPTS_PATH)
-
 clean_up() {
-  test -d "$SCRIPTS_PATH" && rm -fr "$SCRIPTS_PATH"
+  test -d "$SCRIPTS_DIR" && rm -fr "$SCRIPTS_DIR"
 }
 
 trap "clean_up $tmp_dir" EXIT
 
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/setup.sh > $TMP_DIR/setup.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh > $TMP_DIR/post_install.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh > $TMP_DIR/dot_files_config.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $TMP_DIR/colors.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/setup.sh > $SCRIPTS_DIR/setup.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh > $SCRIPTS_DIR/post_install.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh > $SCRIPTS_DIR/dot_files_config.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
 
-. $TMP_DIR/setup.sh
+. $SCRIPTS_DIR/setup.sh
 
