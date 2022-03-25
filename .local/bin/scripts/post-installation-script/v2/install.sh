e753c97ad61182bf3a4c5ed6c2ddedfda4ee7f4b
Martim Lima <martim.d.lima@protonmail.com>
2022-03-25 15:28:13 +0000

    test change to install.sh v2

--- .local/bin/scripts/post-installation-script/v2/install.sh
+++ .local/bin/scripts/post-installation-script/v2/install.sh
@@ -7,11 +7,11 @@ if [ ! -d "$SCRIPTS_DIR" ]; then
 	cd $SCRIPTS_DIR
 fi
 
-cd $SCRIPTS_DIR
+#cd $SCRIPTS_DIR
 
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_init.sh > $SCRIPTS_DIR/dothelper_init.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_install.sh > $SCRIPTS_DIR/dothelper_install.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_config.sh > $SCRIPTS_DIR/dothelper_config.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
 
-./dothelper_init.sh
+source $SCRIPTS_DIR/dothelper_init.sh
