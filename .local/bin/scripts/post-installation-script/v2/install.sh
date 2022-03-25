b321a7fe0ad6ac5841a50dc4a2e089cad249f471
Martim Lima <martim.d.lima@protonmail.com>
2022-03-25 11:51:49 +0000

    Fix to install.sh in Post Installation Scripts v2

--- .local/bin/scripts/post-installation-script/v2/install.sh
+++ .local/bin/scripts/post-installation-script/v2/install.sh
@@ -8,7 +8,7 @@ fi
 
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_init.sh > $SCRIPTS_DIR/dothelper_init.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_install.sh > $SCRIPTS_DIR/dothelper_install.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dothelper_config.sh > $SCRIPTS_DIR/dothelper_config.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_config.sh > $SCRIPTS_DIR/dothelper_config.sh
 curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
 
 . $SCRIPTS_DIR/dothelper_init.sh
