5dc277998ac291e695e44747cb9cabff2b3671a5
Martim Lima <martim.d.lima@protonmail.com>
2022-03-26 00:59:46 +0000

    Fix to install.sh v2

--- .local/bin/scripts/post-installation-script/v2/install.sh
+++ .local/bin/scripts/post-installation-script/v2/install.sh
@@ -8,9 +8,9 @@ fi
 
 #cd $SCRIPTS_DIR
 
-wget -qO https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_init.sh > $SCRIPTS_DIR/dothelper_init.sh
-wget -qO https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_install.sh > $SCRIPTS_DIR/dothelper_install.sh
-wget -qO https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_config.sh > $SCRIPTS_DIR/dothelper_config.sh
-wget -qO https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_init.sh > $SCRIPTS_DIR/dothelper_init.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_install.sh > $SCRIPTS_DIR/dothelper_install.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v2/dothelper_config.sh > $SCRIPTS_DIR/dothelper_config.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
 
 source $SCRIPTS_DIR/dothelper_init.sh
