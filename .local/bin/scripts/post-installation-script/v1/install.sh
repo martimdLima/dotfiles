8b2375a57bee6650412a59dae2af1b889d17c0c2
Martim Lima <martim.d.lima@protonmail.com>
2022-03-26 02:01:51 +0000

    test change to install.sh v1

--- .local/bin/scripts/post-installation-script/v1/install.sh
+++ .local/bin/scripts/post-installation-script/v1/install.sh
@@ -6,9 +6,9 @@ if [ ! -d "$SCRIPTS_DIR" ]; then
 	mkdir $SCRIPTS_DIR
 fi
 
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/setup.sh > $SCRIPTS_DIR/setup.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/post_install.sh > $SCRIPTS_DIR/post_install.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/dot_files_config.sh > $SCRIPTS_DIR/dot_files_config.sh
-curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/colors.sh > $SCRIPTS_DIR/colors.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v1/setup.sh > $SCRIPTS_DIR/setup.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v1/post_install.sh > $SCRIPTS_DIR/post_install.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v1/dot_files_config.sh > $SCRIPTS_DIR/dot_files_config.sh
+curl -Lks https://raw.githubusercontent.com/martimdLima/dotfiles/master/.local/bin/scripts/post-installation-script/v1/colors.sh > $SCRIPTS_DIR/colors.sh
 
 . $SCRIPTS_DIR/setup.sh
